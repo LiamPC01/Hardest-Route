1494a3f14b92a94800f4a3cf7caf1601a3626f09
Oli Paul <olipaul036@gmail.com>
2023-02-05 13:53:11 +0000

    Final Commit Pt2

---
+++ HardestRoute/Intermediate/Build/Win64/HardestRoute/Development/Core/SharedPCH.Core.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30146
+#include "D:/2001555/UE5 Project/Hardest-Route-2/HardestRoute/Intermediate/Build/Win64/HardestRoute/Development/Core/SharedPCH.Core.ShadowErrors.h"
---
+++ HardestRoute/Intermediate/Source/HardestRoute.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class HardestRoute : ModuleRules
+{
+	public HardestRoute(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
